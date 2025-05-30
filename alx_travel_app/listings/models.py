3ae84c0cb60e9161ee16793d14e1cb61ed480229
NahomGulte <100404353+NahomGulte@users.noreply.github.com>
2025-05-30 15:45:08 +0300

    Update models.py

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -2,11 +2,15 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Model):
+class listing(models.Model):
     user_id = models.CharField(max_length=100)
     Name = models.CharField(max_length=100)
     email = models.EmailField()
     age = models.PositiveIntegerField()
-
+class Booking(models.Model):
+    user_id = models.CharField(max_length=100)
+    Name = models.CharField(max_length=100)
+    email = models.EmailField()
+    age = models.PositiveIntegerField()
     def __str__(self):
         return self.name
